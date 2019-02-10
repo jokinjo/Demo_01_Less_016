117da4325333f837cd2c88aa2beb4b9f745dcfbb
JMS <jokinjo2001@hotmail.com>
2019-02-10 12:37:35 -0500

    HelloWorldApp-(?) - L.021 - NO Success w/ fitting all of the buttons on the screen (Button No.1 is pushed off on the left.)

--- app/src/main/java/com/example/demo_01_less_016/MainActivity.java
+++ app/src/main/java/com/example/demo_01_less_016/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.demo_01_less_016;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -13,34 +9,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.linear_layout_mastery);
     }
 
-    CharSequence textN;
-    CharSequence textP;
-    String msg;
-    Integer intCount = 0;
-    Integer resId = 0;
-    Integer caseNo = 0;
-    Context mContext = MainActivity.this;
-
-//    ImageView imgView = null;   // WHY cannot these two lines be above the method scope?
-//    imgView = (ImageView) findViewById(R.id.imgView);
-
-    public void buttonIsClicked(View buttonView) {
-
-        ImageView imgView = null;   // WHY cannot these two lines be above the method scope?
-        imgView = (ImageView) findViewById(R.id.imgView);
-
-        caseNo = intCount++ % 3;
-
-        switch (caseNo) {
-            case 0: resId = (R.drawable.lion);  break;
-            case 1: resId = (R.drawable.tiger);  break;
-            case 2: resId = (R.drawable.bear);  break;
-            default: Toast.makeText(mContext, "caseNo > 2", Toast.LENGTH_SHORT).show();
-        }
-        imgView.setImageResource(resId);
-    }
+//    CharSequence textN;
+//    CharSequence textP;
+//    String msg;
+//    Integer intCount = 0;
+//    Integer resId = 0;
+//    Integer caseNo = 0;
+//    Context mContext = MainActivity.this;
+//
+////    ImageView imgView = null;   // WHY cannot these two lines be above the method scope?
+////    imgView = (ImageView) findViewById(R.id.imgView);
+//
+//    public void buttonIsClicked(View buttonView) {
+//
+//        ImageView imgView = null;   // WHY cannot these two lines be above the method scope?
+//        imgView = (ImageView) findViewById(R.id.imgView);
+//
+//        caseNo = intCount++ % 3;
+//
+//        switch (caseNo) {
+//            case 0: resId = (R.drawable.lion);  break;
+//            case 1: resId = (R.drawable.tiger);  break;
+//            case 2: resId = (R.drawable.bear);  break;
+//            default: Toast.makeText(mContext, "caseNo > 2", Toast.LENGTH_SHORT).show();
+//        }
+//        imgView.setImageResource(resId);
+//    }
 
 }
