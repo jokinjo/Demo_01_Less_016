8e63de4d8697641b26d9b2abd333ac5e9da3cdab
JMS <jokinjo2001@hotmail.com>
2019-02-10 03:28:06 -0500

    HelloWorldApp-3 - L.019 - Success w/ Showing toast msgs at particular place on the screen.  App Finished!

--- app/src/main/java/com/example/demo_01_less_016/MainActivity.java
+++ app/src/main/java/com/example/demo_01_less_016/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.demo_01_less_016;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -16,17 +18,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    String textN = "";
-    String textP = "";
+    CharSequence textN;
+    CharSequence textP;
+    String msg;
 
     public void buttonIsClicked(View buttonView) {
         TextView edtName = findViewById(R.id.edtName);
         textN = edtName.getText().toString();
 
         EditText edtPhone = findViewById(R.id.edtPhone);
-        textP = edtPhone.getText().toString();
+        textP = edtPhone.getText();
 
-        Log.i(TAG, "buttonIsClicked - Name is: " + textN + "  Phone is: " + textP);
+
+        Context context = MainActivity.this;
+        Toast toastN = null;
+        Toast toastP = null;
+
+        toastN = toastN.makeText(context, textN, Toast.LENGTH_SHORT);
+        toastN.setGravity(Gravity.TOP, 0, 700);       //yOffset = [(420dpi / 1920px) * 5.5in]^-1 * 420dpi * yInches_to_offset
+        toastN.show();
+
+        toastP = toastP.makeText(context, textP, Toast.LENGTH_SHORT);
+        toastP.setGravity(Gravity.TOP, 0, 875);
+        toastP.show();
+
     }
 
 }
